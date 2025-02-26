9c8bc273f1f602a5afd316ac44d3182a6263137a
diba1013 <diba1013@web.de>
2025-02-26 22:19:43 +0100

    chore(deps): update dependencies
    Signed-off-by: diba1013 <diba1013@web.de>

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "@diba1013/eslint-config";
+import { defineConfig } from "@diba1013/linter/eslint";
 
 export default defineConfig({
-	environment: "browser",
+	platform: "node",
 	typescript: "./tsconfig.json",
 });
